5d251ad8776008de1bfe85b4ca419b0abc54616e
Andrey Patsalyuk <pazalyuk@gmail.com>
2020-12-09 11:45:34 +0300

    Empty application

---
+++ src/main/java/com/github/patsalyukas/Main.java
@@ -0,0 +1,12 @@
+package com.github.patsalyukas;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+
+    public static void main(String[] args) {
+        log.info("Start application.");
+    }
+
+}
